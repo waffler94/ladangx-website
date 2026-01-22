1357cf5d0fa7f6ef2d7c0e18aed794624fb65362
harng <wangth94@gmail.com>
2026-01-22 15:04:12 +0800

    commit

--- components/language-globe.js
+++ components/language-globe.js
@@ -1,5 +1,4 @@
 'use client'
-import { Globe } from 'lucide-react'
 import { useTranslations } from 'next-intl'
 import React, { useContext } from 'react'
 import { modalList, PopupContext } from './context/PopupProvider'
@@ -12,7 +11,7 @@ export default function LanguageGlobe() {
     }
     return (
         <button onClick={handleOpenLanguageModal} className="hover:scale-110 transition-all text-[24px] size-[40px] bg-[#446A2A] rounded-full flex border-[3px] shadow-[0px_4px_0px_0px_rgba(57,83,39,1)] border-white items-center justify-center size ml-auto hover:cursor-pointer">
-            <Globe className="text-[#446A2A] bg-white rounded-full" />
+            <i className="icon-language text-[#446A2A] bg-white rounded-full" />
         </button>
     )
 }
