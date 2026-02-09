bf83ce644607385ba6afaa91e79a066fd394c19a
Tomato Wilbur <yapxh2004@hotmail.com>
2026-02-09 05:00:55 +0000

    update

--- app/[locale]/(dashboard)/stamp-ques/page.js
+++ app/[locale]/(dashboard)/stamp-ques/page.js
@@ -1,14 +1,20 @@
 'use client'
 import BackButton from "@/components/back-button";
+import Pagination from "@/components/pagination";
 import { Link } from "@/i18n/navigation";
 import { useGetUserQuizStatus } from "@/lib/hooks/useGetUserQuizStatus";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
+import { useSearchParams } from "next/navigation";
 import React from "react";
 
 export default function page() {
     const t = useTranslations();
+    const searchParams = useSearchParams();
+    const page = searchParams.get("page") || 1;
+    const itemsPerPage = 20;
     const { data: quizData, isLoading: isLoadingQuiz } = useGetUserQuizStatus();
+    const totalPages = quizData ? Math.ceil(quizData.data.length / itemsPerPage) : 0;
 
     return (
         <div className="bg-[url('/images/bg7-stampques.png')] bg-cover bg-[50%_0px]  min-h-screen relative pb-[120px] pt-safe">
@@ -21,11 +27,12 @@ export default function page() {
                 </h1>
             </div>
             <div className="mt-[210px] w-full px-[20px]    ">
-                <div>
+                <div className="relative">
+                    <i className="icon-search text-[24px] absolute left-[15px] top-1/2 -translate-y-1/2 text-gray-400" />
                     <input
                         type="text"
                         placeholder={t("search_ticket_placeholder")}
-                        className=" w-full h-[44px] rounded-full px-[15px] bg-white/90 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        className=" w-full h-[44px] rounded-full px-[45px] outline-none bg-white/90 placeholder:text-gray-400 focus:outline-none "
                     />
                 </div>
                 <div className="grid grid-cols-5 w-full gap-x-[14px] gap-y-[16px] mt-[16px]">
@@ -58,6 +65,10 @@ export default function page() {
                         ))
                     )}
                 </div>
+                <Pagination current_page={parseInt(page)} items_per_page={itemsPerPage}
+                    total_pages={totalPages}
+
+                />
             </div>
         </div>
     );
