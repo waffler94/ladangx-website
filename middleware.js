5556897a12618b200697f823b5c2a0ed861f8765
minegrass <yapxh2004@hotmail.com>
2026-06-11 14:02:48 +0000

    update

--- middleware.js
+++ middleware.js
@@ -8,11 +8,11 @@ export function middleware(request) {
     const isMobileAppClient = request.headers.get("x-app-client") === "mobile";
 
 
-    // if (!isDevEnv && !isMobileAppClient) {
-    //     const downloadUrl = new URL("/download-redirect", request.url);
-    //     downloadUrl.searchParams.set("reason", "browser");
-    //     return NextResponse.redirect(downloadUrl);
-    // }
+    if (!isDevEnv && !isMobileAppClient) {
+        const downloadUrl = new URL("/download-redirect", request.url);
+        downloadUrl.searchParams.set("reason", "browser");
+        return NextResponse.redirect(downloadUrl);
+    }
 
     const response = handleI18nRouting(request);
     response.headers.set("x-pathname", request.nextUrl.pathname);
